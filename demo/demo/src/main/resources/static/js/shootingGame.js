54bb027bfc7a2bf932eeaff4075ddb7f624724a1
lgh000322 <kang000322@gmail.com>
2024-08-09 16:17:59 +0900

    Feat: 슈팅게임의 랭킹, 댓글기능 추가

--- demo/demo/src/main/resources/static/js/shootingGame.js
+++ demo/demo/src/main/resources/static/js/shootingGame.js
@@ -1,205 +1,321 @@
 $(document).ready(function () {
+
+    let serverScore;
+
+    function commentWrite() {
+        let content = $("#comment").val();
+        let gamename = $("title").text();
+        let username = $('#memberName').text();
+
+        fetch("/comment/write", {
+            method: "POST",
+            body: JSON.stringify({ content: content, gamename: gamename }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    // 댓글 작성 성공 시 데이터를 추가
+                    let newComment = `
+                        <div class="comments">
+                            <p class="userName">${username}</p>
+                            <p class="userComment">${content}</p>
+                        </div>
+                    `;
+                    $("#comments-aria").prepend(newComment);
+                    $("#comment").val("");
+                } else {
+                    alert("댓글 작성 중 에러가 발생했습니다.");
+                }
+            })
+            .catch(error => {
+                console.error("Error: ", error);
+                alert("댓글 작성 중 에러가 발생했습니다.");
+            });
+    }
+
+    function getUserScore() {
+        let gamename = $("title").text();
+        let username = $('#memberName').text();
+
+        return fetch(`/ranking/score?gamename=${gamename}&nickname=${username}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                return data.score;
+            })
+            .catch(error => {
+                console.error("Error: ", error);
+                alert("회원의 점수를 가져오는데 에러가 발생했습니다.");
+                return 0;
+            });
+    }
+
+    function updateScore(score) {
+            let gamename = $("title").text();
+            let username = $('#memberName').text();
+
+            fetch("/ranking/score", {
+                method: "POST",
+                body: JSON.stringify({ gamename: gamename, nickname: username, score: score }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        console.log("데이터 업데이트 성공" + data.score)
+                    } else {
+                        alert("회원의 점수를 가져오는데 실패했습니다.");
+                    }
+                })
+                .catch(error => {
+                    console.error("Error: ", error);
+                    alert("회원의 점수를 가져오는데 에러가 발생했습니다.");
+                });
+        }
+
+
+    $("#comment-submit-btn").click(function () {
+        commentWrite();
+    });
+
+    let text = ''; // 댓글을 담을 텍스트
+
+    let setText = function () {
+        text += ' <div class="comments">';
+        text += '  <p class="userName">id</p>';
+        text += ' <p class="userComment"> comment......</p>';
+        text += '</div>';
+    };
+
+    let appendComment = function () {
+        for (var i = 0; i < 20; i++) {
+            $("#comments-aria").append(text);
+        }
+    };
+
+    $(window).scroll(function () {
+        let scrollHeight = $(window).scrollTop() + $(window).height();
+        let documentHeight = $(document).height();
+
+        if (scrollHeight >= documentHeight - 100) {
+            appendComment();
+        }
+    });
+
     //캔버스
     function start() {
-        let canvas = $("canvas").get(0);
-        let ctx = canvas.getContext("2d");
-        let backgroundImage, spaceShipImage, bulletImage, enemyImage, gameOverImage;
-        let spaceShipX = canvas.width / 2 - 15;
-        let spaceShipY = canvas.height - 30;
-        let spaceShipXSize = 30;
-        let spaceShipYSize = 30;
-        let spaceShipXSpeed = 3;
-        let keysDown = {}
-        let bulletList = [];
-        let enemyList = [];
-        let gameOver = false;
-        let score = 0;
-        let intervalId;
+        getUserScore().then(serverScore => {
+            let canvas = $("canvas").get(0);
+            let ctx = canvas.getContext("2d");
 
-        function loadImage() {
-            backgroundImage = new Image();
-            backgroundImage.src = "../img/backGroundImage.jpg"
+            let backgroundImage, spaceShipImage, bulletImage, enemyImage, gameOverImage;
+            let spaceShipX = canvas.width / 2 - 15;
+            let spaceShipY = canvas.height - 30;
+            let spaceShipXSize = 30;
+            let spaceShipYSize = 30;
+            let spaceShipXSpeed = 3;
+            let keysDown = {};
+            let bulletList = [];
+            let enemyList = [];
+            let gameOver = false;
+            let score = 0;
+            let intervalId;
 
-            spaceShipImage = new Image();
-            spaceShipImage.src = "../img/player.png";
+            function loadImage() {
+                backgroundImage = new Image();
+                backgroundImage.src = "../img/backGroundImage.jpg";
 
-            bulletImage = new Image();
-            bulletImage.src = "../img/bullet.png";
+                spaceShipImage = new Image();
+                spaceShipImage.src = "../img/player.png";
 
-            enemyImage = new Image();
-            enemyImage.src = "../img/enemy.png"
+                bulletImage = new Image();
+                bulletImage.src = "../img/bullet.png";
 
-            gameOverImage = new Image();
-            gameOverImage.src = "../img/gameOverImage.png";
-        }
+                enemyImage = new Image();
+                enemyImage.src = "../img/enemy.png";
 
-        function render() {
-            ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-            ctx.drawImage(spaceShipImage, spaceShipX, spaceShipY, spaceShipXSize, spaceShipYSize)
-            ctx.fillText(`Score:${score}`, 20, 20);
-            ctx.fillStyle = "white"
-            ctx.font = "20px Arial"
+                gameOverImage = new Image();
+                gameOverImage.src = "../img/gameOverImage.png";
+            }
 
-            for (let i = 0; i < bulletList.length; i++) {
-                if (bulletList[i].alive) {
-                    ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y, 10, 10)
+            function render() {
+                ctx.clearRect(0, 0, canvas.width, canvas.height); // 캔버스 초기화
+                ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+                ctx.drawImage(spaceShipImage, spaceShipX, spaceShipY, spaceShipXSize, spaceShipYSize);
+                ctx.fillText(`Score:${score}`, 20, 20);
+                ctx.fillStyle = "white";
+                ctx.font = "20px Arial";
+
+                for (let i = 0; i < bulletList.length; i++) {
+                    if (bulletList[i].alive) {
+                        ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y, 10, 10);
+                    }
                 }
-            }
 
-            for (let i = 0; i < enemyList.length; i++) {
-                ctx.drawImage(enemyImage, enemyList[i].x, enemyList[i].y, 30, 30)
-            }
-
-
-
-        }
-
-        function main() {
-            if (!gameOver) {
-                update();
-                render();
-                requestAnimationFrame(main)
-            } else {
-                ctx.drawImage(gameOverImage, canvas.width / 2 - 43, 10, 100, 100)
-
-                console.log(bulletList)
-            }
-        }
-
-        function setupKeyboardListener() {
-            $(document).keydown(function (event) {
-                keysDown[event.keyCode] = true;
-                console.log("버튼 클릭", keysDown)
-            })
-
-            $(document).keyup(function (event) {
-                delete keysDown[event.keyCode]
-
-                if (event.keyCode == 17) {
-                    createBullet();
-                }
-            });
-        }
-
-        function createBullet() {
-            console.log("총알 생성")
-            let b = new Bullet();
-            b.init();
-            console.log("총알 리스트:", bulletList)
-
-        }
-
-        function Bullet() {
-            this.x = 0;
-            this.y = 0;
-            this.alive = true;
-
-            this.init = function () {
-                this.x = spaceShipX + 10;
-                this.y = spaceShipY;
-                bulletList.push(this);
-            }
-
-            this.update = function () {
-                if (this.y <= 30) {
-                    this.alive = false;
-                }
-                this.y -= 7;
-            }
-
-            this.checkHit = function () {
                 for (let i = 0; i < enemyList.length; i++) {
-                    if (this.y <= enemyList[i].y && this.x >= enemyList[i].x && this.x <= enemyList[i].x + 30) {
-                        score++;
-                        this.alive = false;
-                        enemyList.splice(i, 1);
+                    ctx.drawImage(enemyImage, enemyList[i].x, enemyList[i].y, 30, 30);
+                }
+            }
+
+            function main() {
+                if (!gameOver) {
+                    update();
+                    render();
+                    requestAnimationFrame(main);
+                } else {
+                    ctx.drawImage(gameOverImage, canvas.width / 2 - 43, 10, 100, 100);
+                    clearInterval(intervalId); // 적 생성 중지
+                    if(score>serverScore){
+                        updateScore(score);
                     }
                 }
             }
-        }
 
-        function update() {
-            //right
-            if (39 in keysDown) {
-                spaceShipX += spaceShipXSpeed;
+            function setupKeyboardListener() {
+                $(document).off('keydown').on('keydown', function (event) {
+                    keysDown[event.keyCode] = true;
+                });
+
+                $(document).off('keyup').on('keyup', function (event) {
+                    delete keysDown[event.keyCode];
+
+                    if (event.keyCode == 17) {
+                        createBullet();
+                    }
+                });
             }
 
-            //left
-            if (37 in keysDown) {
-                spaceShipX -= spaceShipXSpeed;
+            function createBullet() {
+                let b = new Bullet();
+                b.init();
             }
 
-            if (spaceShipX <= 0) {
-                spaceShipX = 0;
+            function Bullet() {
+                this.x = 0;
+                this.y = 0;
+                this.alive = true;
+
+                this.init = function () {
+                    this.x = spaceShipX + 10;
+                    this.y = spaceShipY;
+                    bulletList.push(this);
+                };
+
+                this.update = function () {
+                    if (this.y <= 30) {
+                        this.alive = false;
+                    }
+                    this.y -= 7;
+                };
+
+                this.checkHit = function () {
+                    for (let i = 0; i < enemyList.length; i++) {
+                        if (this.y <= enemyList[i].y && this.x >= enemyList[i].x && this.x <= enemyList[i].x + 30) {
+                            score++;
+                            this.alive = false;
+                            enemyList.splice(i, 1);
+                        }
+                    }
+                };
             }
 
-            if (spaceShipX >= canvas.width - spaceShipXSize) {
-                spaceShipX = canvas.width - spaceShipXSize;
-            }
+            function update() {
+                // 오른쪽
+                if (39 in keysDown) {
+                    spaceShipX += spaceShipXSpeed;
+                }
 
-            for (let i = 0; i < bulletList.length; i++) {
-                if (bulletList[i].alive) {
-                    bulletList[i].update();
-                    bulletList[i].checkHit();
-                } else {
-                    bulletList.splice(i, 1)
+                // 왼쪽
+                if (37 in keysDown) {
+                    spaceShipX -= spaceShipXSpeed;
+                }
+
+                if (spaceShipX <= 0) {
+                    spaceShipX = 0;
+                }
+
+                if (spaceShipX >= canvas.width - spaceShipXSize) {
+                    spaceShipX = canvas.width - spaceShipXSize;
+                }
+
+                for (let i = 0; i < bulletList.length; i++) {
+                    if (bulletList[i].alive) {
+                        bulletList[i].update();
+                        bulletList[i].checkHit();
+                    } else {
+                        bulletList.splice(i, 1);
+                    }
+                }
+
+                for (let i = 0; i < enemyList.length; i++) {
+                    enemyList[i].update();
                 }
             }
 
-            for (let i = 0; i < enemyList.length; i++) {
-                enemyList[i].update();
+            function generateRandomValue(min, max) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
             }
 
-
-        }
-
-        function generateRandomValue(min, max) {
-            let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-            return randomNumber;
-        }
-
-        function createEnemy() {
-            if (!gameOver) {
+            function createEnemy() {
                 intervalId = setInterval(function () {
-                    let e = new Enemy();
-                    e.init();
-                }, 1000)
+                    if (!gameOver) {
+                        let e = new Enemy();
+                        e.init();
+                    } else {
+                        clearInterval(intervalId);
+                    }
+                }, 1000);
             }
-            else {
-                clearInterval(intervalId);
-            }
 
-        }
-
-        function Enemy() {
-            this.x = 0;
-            this.y = 0;
-
-            this.init = function () {
-                this.x = generateRandomValue(0, canvas.width - 30);
+            function Enemy() {
+                this.x = 0;
                 this.y = 0;
 
-                enemyList.push(this);
+                this.init = function () {
+                    this.x = generateRandomValue(0, canvas.width - 30);
+                    this.y = 0;
+
+                    enemyList.push(this);
+                };
+
+                this.update = function () {
+                    this.y += 1;
+
+                    if (this.y >= canvas.height - 30) {
+                        gameOver = true;
+                    }
+                };
             }
 
-            this.update = function () {
-                this.y += 1;
+            // 게임 상태 초기화
+            gameOver = false;
+            score = 0;
+            bulletList = [];
+            enemyList = [];
 
-                if (this.y >= canvas.height - 30) {
-                    gameOver = true;
-                }
-            }
-        }
-
-        loadImage();
-        createEnemy();
-        setupKeyboardListener();
-        main();
+            loadImage();
+            createEnemy();
+            setupKeyboardListener();
+            main();
+        });
     }
 
     start();
+    setText();
 
-    $("button").on("click",function(){
+    $("#restart").on("click", function () {
         start();
-    })
+    });
 });
